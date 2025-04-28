d0c239773c09089a525749b724b2fd0ef7bac0cb
刘明 <mingliu63@creditease.cn>
2025-04-28 15:24:19 +0800

    feat: integrate Framework7 and Tailwind CSS into the React app
    - Added Framework7 and Framework7-React dependencies to package.json
    - Included Tailwind CSS and its PostCSS plugin for styling
    - Updated App component to use Framework7 for routing and layout
    - Created About and Action pages with respective components
    - Implemented actions and buttons in Action page for user interaction
    - Refactored App.css to utilize Tailwind CSS for styling
    - Added PostCSS configuration for Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { App, Link, Navbar, Page, Toolbar, View } from 'framework7-react';
 import './App.css';
 
-const App = () => {
+const AppMain = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <App theme="auto" name="My App" routes={[
+      {
+        path: '/about',
+        asyncComponent: () => import('./pages/About')
+      },
+      {
+        path: '/action',
+        asyncComponent: () => import('./pages/Action')
+      }
+    ]}>
+      {/* Your main view, should have "main" prop */}
+      <View main>
+        {/*  Initial Page */}
+        <Page>
+          {/* Top Navbar */}
+          {/* <Navbar title="Awesome App"></Navbar> */}
+          {/* Toolbar */}
+          <Toolbar bottom>
+            <Link href="/about">About Page</Link>
+            <Link href="/action">Action Page</Link>
+          </Toolbar>
+          {/* Page Content */}
+          <p>Page content goes here</p>
+          <Link href="/about/">About App</Link>
+        </Page>
+      </View>
+    </App>
   );
 };
 
-export default App;
+export default AppMain;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Import Framework7 Core
+import Framework7 from 'framework7/lite';
+
+import 'framework7/css/bundle';
+/*
+Or import bundle with all components:
+import Framework7 from 'framework7/lite-bundle';
+*/
+
+// Import Framework7 React
+import Framework7React from 'framework7-react';
+import ActionsComponent from 'framework7/components/actions';
+
+// Init plugin
+Framework7.use([Framework7React, ActionsComponent]);
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+  root.render(<App />);
 }
